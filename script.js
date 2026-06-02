f07e5c7e86b7eb0f80ae574477a91c37f44a9d98
anshsharma1654456-ui <anshsharma1654456@gmail.com>
2026-06-02 05:37:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,36 @@ async function generateImage() {
 
     const prompt = document.getElementById("prompt").value;
     const result = document.getElementById("result");
+    const ratio = document.getElementById("aspectRatio").value;
 
     if (!prompt.trim()) {
         alert("Please enter a prompt");
         return;
     }
 
-    result.innerHTML = "<h2>Generating AI Image...</h2>";
+    result.innerHTML = `
+        <h2>Generating AI Image...</h2>
+        <br>
+        <p>Please wait a few seconds...</p>
+    `;
+
+    const width = ratio.split("x")[0];
+    const height = ratio.split("x")[1];
 
     const imageUrl =
-        `https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}`;
+        `https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}?width=${width}&height=${height}`;
 
     result.innerHTML = `
         <img src="${imageUrl}" alt="AI Generated Image">
+
+        <br>
+
+        <a
+            class="download-btn"
+            href="${imageUrl}"
+            target="_blank"
+        >
+            Download Image
+        </a>
     `;
 }
